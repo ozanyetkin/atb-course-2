52fc00d035ab83f42894c18ef7e8acd5dac3dfb4
Ozan Yetkin <ozan.yetkin@metu.edu.tr>
2022-08-20 13:42:57 +0300

    syntax basics finished

---
+++ 01_syntax/operators.py
@@ -0,0 +1,20 @@
+num_1 = 10
+num_2 = 3
+
+summation = num_1 + num_2
+subtraction = num_1 - num_2
+multiplication = num_1 * num_2
+division = num_1 / num_2
+
+int_division = num_1 // num_2
+modulo = num_1 % num_2
+exponentiation = num_1 ** num_2
+square_root = num_1 ** (1/2)
+
+print(summation)
+print(subtraction)
+print(multiplication)
+print(division)
+print(int_division)
+print(exponentiation)
+print(square_root)
---
+++ 01_syntax/variables.py
@@ -0,0 +1,29 @@
+# int
+a_number = 5
+# str
+a_string = "kelime veya cümle"
+# float
+a_float = 3.2
+# bool
+a_bool = True
+
+# list
+a_list = [5, 3, 7, 5]
+b_list = ["a", 5, 3.2]
+
+# set
+a_set = {5, 3, 7, 5}
+
+# dict
+a_dict = {"name": "ozan"}
+
+print(a_number)
+print(a_string)
+print(a_float)
+print(a_bool)
+
+print(a_list)
+print(b_list)
+
+print(a_set)
+print(a_dict)
