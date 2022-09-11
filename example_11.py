41032717ca7e971b928c4d12dc970813bf006fc2
Ozan Yetkin <ozan.yetkin@metu.edu.tr>
2022-09-11 12:20:37 +0300

    example 11 finished, visualization to be done

--- example_11.py
+++ example_11.py
@@ -10,10 +10,12 @@ with open('./inputs/grades.txt') as f:
 # "\t" == "Tab" (iceri gir)
 # "\b" == "Backspace" (sil)
 # print(lines)
+# student_list her ogrencinin bilgilerini tek listede tutacak
 student_list = []
 for line in lines:
     student_info = line.replace("\n", "").split(",")
     # print(student_info)
+    # info_list her bir ogrencinin tum bilgilerini icerecek
     info_list = []
     for info in student_info:
         info = info.strip()
@@ -22,5 +24,35 @@ for line in lines:
         # print(type(info))
         info_list.append(info)
     student_list.append(info_list)
+
+print(student_list)
+
+total_grades = 0
+max_grade = 0
+min_grade = 100
+for student in student_list:
+    total_grades += student[-1]
+    if student[-1] > max_grade:
+        max_grade = student[-1]
+    if student[-1] < min_grade:
+        min_grade = student[-1]
+
+average_grade = total_grades / len(student_list)
+min_A = average_grade + (max_grade - min_grade) / 2
+max_F = average_grade - (max_grade - min_grade) / 2
+min_B = min_A - (min_A - max_F) / 3
+min_C = max_F + (min_A - max_F) / 3
+
+for student in student_list:
+    if student[-1] <= max_F:
+        student.append("F")
+    elif max_F < student[-1] < min_C:
+        student.append("D")
+    elif min_C <= student[-1] < min_B:
+        student.append("C")
+    elif min_B <= student[-1] < min_A:
+        student.append("B")
+    else:
+        student.append("A")
 
 print(student_list)
