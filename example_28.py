91db53f29c1ca94b8a41bda20f9d211fd1e9df2c
Ozan Yetkin <ozan.yetkin@metu.edu.tr>
2022-10-16 12:26:25 +0300

    diabetes data upped, problem explained

---
+++ example_28.py
@@ -0,0 +1,7 @@
+# diabetes_data.txt dosyasini oku
+# toplam hasta sayisi
+# yasi 55'ten buyuk olanlarin ortalama vucut kutle endeksi (Body_mass_index)
+# kac kisinin kandaki glikoz oraninin (glucose_concentration) 105'ten buyuk oldugu
+# her bir kolon icin kac verinin eksik oldugunu bulacagiz
+# yasi 20 ve 30 arasinda olan kisilerin minimum ve maksimum kan basinci ve serum insulin degerleri
+# kullanicidan yas araligi alarak bu yas araliginda olan herkesin tum degerlerini gosterecegiz
